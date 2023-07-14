05195c19bd8d326161c4b07b257bb5813259896a
Jacob <jacoblower4@gmail.com>
2023-07-14 13:24:19 -0400

    slight changes

--- WUG/Managers/GDPManager.cs
+++ WUG/Managers/GDPManager.cs
@@ -2,26 +2,6 @@
 
 public static class GDPManager
 {
-    /// <summary>
-    /// Item Def Id: price
-    /// </summary>
-    public static Dictionary<long, decimal> CurrentPricesFromNVSE { get; set; }
-
-    public static void AddPrice(string resource, decimal price)
-    {
-        CurrentPricesFromNVSE[GameDataManager.ResourcesToItemDefinitions[resource].Id] = price;
-    }
-
-    public static async Task UpdatePricesAsync()
-    {
-        CurrentPricesFromNVSE = new();
-        AddPrice("iron", 0.4m);
-        AddPrice("coal", 0.6m);
-        AddPrice("iron_ore", 0.2m);
-        AddPrice("copper", 0.8m);
-        AddPrice("copper_ore", 0.3m);
-    }
-
     public static decimal GetGDPOfBuilding(ProducingBuilding building)
     {
         decimal total = 0.0m;
@@ -33,7 +13,7 @@ public static class GDPManager
         }
         foreach (var pair in building.Recipe.Outputs)
         {
-            if (CurrentPricesFromNVSE.ContainsKey(pair.Key))
+            if (PriceManager.ContainsKey(pair.Key))
             {
                 total += (decimal)pair.Value * (decimal)building.GetRateForProduction() * CurrentPricesFromNVSE[pair.Key];
             }
