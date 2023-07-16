198a08dd0542391fb4414abba7e3c09e12e19cb2
Jacob <jacoblower4@gmail.com>
2023-07-16 17:59:43 -0400

    f

--- WUG/Controllers/AccountController.cs
+++ WUG/Controllers/AccountController.cs
@@ -21,7 +21,7 @@ public class AccountController : SVController {
 #if DEBUG
     private static string Redirecturl = "https://localhost:7186/callback";
 #else
-    private static string Redirecturl = "https://dev.spookvooper.com/callback";
+    private static string Redirecturl = "https://wug.superjacobl.com/callback";
     //private static string Redirecturl = "https://spookvooper.com/callback";
 #endif
 	private readonly ILogger<AccountController> _logger;
